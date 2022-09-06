9b64d5d4035ad8be0f181227363d5db9bca8f443
NateTewolde <tewolde.nate@gmail.com>
2022-09-05 21:54:46 -0700

    Partially complete dot navigation

--- image-slider.js
+++ image-slider.js
@@ -12,27 +12,40 @@ function moveToRight() {
   currentImage = document.querySelector(".current-img");
   currentImgId = currentImage.getAttribute("data-img-id").slice(-1);
   if (currentImgId === "4") {
-    return;
+    currentImgId = "-1";
   }
   currentImage.classList.remove("current-img");
   nextImage = document.querySelector(
     "[data-img-id=image" + (+currentImgId + 1) + "]"
   );
   nextImage.classList.add("current-img");
+
+  currentDot = document.querySelector("[data-dot-id=dot" + +currentImgId + "]");
+  currentDot.classList.remove("current-dot");
+  nextDot = document.querySelector(
+    "[data-dot-id=dot" + (+currentImgId + 1) + "]"
+  );
+  nextDot.classList.add("current-dot");
 }
 
 function moveToLeft() {
   currentImage = document.querySelector(".current-img");
   currentImgId = currentImage.getAttribute("data-img-id").slice(-1);
-  if (currentImgId < 1) {
-    return;
+  if (currentImgId === "0") {
+    currentImgId = "5";
   }
   currentImage.classList.remove("current-img");
-
   nextImage = document.querySelector(
     "[data-img-id=image" + (+currentImgId - 1) + "]"
   );
   nextImage.classList.add("current-img");
+
+  currentDot = document.querySelector("[data-dot-id=dot" + +currentImgId + "]");
+  currentDot.classList.remove("current-dot");
+  nextDot = document.querySelector(
+    "[data-dot-id=dot" + (+currentImgId - 1) + "]"
+  );
+  nextDot.classList.add("current-dot");
 }
 
 // const element = document.querySelector(".current-img");
