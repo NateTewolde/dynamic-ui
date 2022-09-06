672334ab7fbc691683f7a837fa14d737b81d05ab
NateTewolde <tewolde.nate@gmail.com>
2022-09-05 20:52:40 -0700

    Add working image gallery, able to go through the images left or right

--- image-slider.js
+++ image-slider.js
@@ -2,30 +2,44 @@ formatArrows();
 
 function formatArrows() {
   let rightArrow = document.querySelector(".right-arrow");
-  rightArrow.addEventListener("click", moveToRight());
+  rightArrow.addEventListener("click", moveToRight);
 
   let leftArrow = document.querySelector(".left-arrow");
-  leftArrow.addEventListener("click", moveToLeft());
+  leftArrow.addEventListener("click", moveToLeft);
 }
 
 function moveToRight() {
   currentImage = document.querySelector(".current-img");
-  currentImage.classList.remove(".current-img");
-  currentImgId = currentImage.getAttribute("data-img");
-  if (currentImgId > 5) {
+  currentImgId = currentImage.getAttribute("data-img-id").slice(-1);
+  if (currentImgId === "4") {
     return;
   }
-  nextImage = document.querySelector("[data-img=" + currentImgId + 1 + "]");
+  currentImage.classList.remove("current-img");
+  nextImage = document.querySelector(
+    "[data-img-id=image" + (+currentImgId + 1) + "]"
+  );
   nextImage.classList.add("current-img");
 }
 
 function moveToLeft() {
   currentImage = document.querySelector(".current-img");
-  currentImage.classList.remove(".current-img");
-  currentImgId = currentImage.getAttribute("data-img");
+  currentImgId = currentImage.getAttribute("data-img-id").slice(-1);
   if (currentImgId < 1) {
     return;
   }
-  nextImage = document.querySelector("[data-img=" + currentImgId - 1 + "]");
+  currentImage.classList.remove("current-img");
+
+  nextImage = document.querySelector(
+    "[data-img-id=image" + (+currentImgId - 1) + "]"
+  );
   nextImage.classList.add("current-img");
 }
+
+// const element = document.querySelector(".current-img");
+
+// const attrs = element.getAttributeNames().reduce((acc, name) => {
+//   return { ...acc, [name]: element.getAttribute(name) };
+// }, {});
+
+// // 👇️ {id: 'blue', 'data-id': 'example', class: 'box'}
+// console.log(attrs);
